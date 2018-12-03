01ca8b36bf83eeacd3d196aa5e39e697c5ebb569
Monique <moniquechan1220@gmail.com>
2018-12-03 14:12:05 +0800

    finish task 2.

--- src/index.js
+++ src/index.js
@@ -4,6 +4,6 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<button>Hello world!</button>, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 
 serviceWorker.unregister();
