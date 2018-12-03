3da84ccd3c5f8412cd1b95f98eb49f0761a082c6
Monique <moniquechan1220@gmail.com>
2018-12-03 14:26:03 +0800

    finished task 3.

--- src/App.js
+++ src/App.js
@@ -2,10 +2,13 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  onClickAlert = () =>{
+    alert("now you know how to handle onclick event");
+  }
   render() {
     return (
       <div className="App">
-        <button>Hello world!</button>
+        <button onClick={this.onClickAlert}>Hello world!</button>
       </div>
     );
   }
